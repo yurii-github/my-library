ca261e3b9c3dc21364d7f53a72e78e1b6861beb1
Yurii K. <yurii-github@users.noreply.github.com>
2015-06-11 15:36:03 +0300

    [skip ci]

--- .travis-install.sh
+++ .travis-install.sh
@@ -4,7 +4,7 @@ if [ -d vendor/bin ]
 then
   echo -e "\e[0;34;40m using cache. nothing to do";
 else
-  echo "\e[0;34;40m getting latest PHPUnit";
+  echo -e "\e[0;34;40m getting latest PHPUnit";
   wget https://phar.phpunit.de/phpunit.phar -O yurii/phpunit.phar --no-check-certificate
   echo -e "\e[0;34;40m removing dev deps as we have ones in CI or not required for testing";
   composer remove almasaeed2010/adminlte --no-update

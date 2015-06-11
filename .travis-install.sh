7b4beee826f6ef1738ea94097a4620ccd38858f3
Yurii K <y.korotia@hotmail.com>
2015-06-11 12:26:28 +0300

    travis ci

--- .travis-install.sh
+++ .travis-install.sh
@@ -2,9 +2,11 @@
 
 if [ -d vendor/bin ]
 then
-  echo 'using cache. nothing to do.';
+  echo "using cache. nothing to do";
 else
- echo 'downloading dependencies..';
- composer install --prefer-dist
- composer require codeclimate/php-test-reporter
+  echo "removing phpunit as we have once in CI";
+  composer remove phpunit/phpunit phpunit/dbunit --no-update  
+  echo "downloading dependencies...";
+  composer install --prefer-dist
+  composer require codeclimate/php-test-reporter --prefer-dist
 fi

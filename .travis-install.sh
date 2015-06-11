ce9991ee8ff98c702ef147ed50cd91b37a156980
Yurii K. <yurii-github@users.noreply.github.com>
2015-06-12 00:16:14 +0300

    combine to one cache file

--- .travis-install.sh
+++ .travis-install.sh
@@ -5,7 +5,7 @@ then
   echo -e "\e[0;34;40m using cache. nothing to do";
 else
   echo -e "\e[0;34;40m getting latest PHPUnit";
-  wget https://phar.phpunit.de/phpunit.phar -O yurii/phpunit.phar --no-check-certificate
+  wget https://phar.phpunit.de/phpunit.phar -O vendor/phpunit.phar --no-check-certificate
   echo -e "\e[0;34;40m removing dev deps as we have ones in CI or not required for testing";
   composer remove almasaeed2010/adminlte --no-update
   composer remove yiisoft/yii2-debug phpunit/phpunit phpunit/dbunit --dev --no-update

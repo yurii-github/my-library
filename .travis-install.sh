1ef3ed324a2e774e047990c2addcdab4cfc9131e
Yurii K <y.korotia@hotmail.com>
2015-06-11 13:45:37 +0300

    travis

--- .travis-install.sh
+++ .travis-install.sh
@@ -2,14 +2,14 @@
 
 if [ -d vendor/bin ]
 then
-  echo "using cache. nothing to do";
+  echo "\e[43musing cache. nothing to do";
 else
-  echo "getting latest PHPUnit";
-  wget https://phar.phpunit.de/phpunit.phar -O phpunit.phar -P yurii
-  echo "removing dev deps as we have ones in CI or not required for testing";
+  echo "\e[43mgetting latest PHPUnit";
+  wget https://phar.phpunit.de/phpunit.phar -O yurii/phpunit.phar --no-check-certificate
+  echo "\e[43mremoving dev deps as we have ones in CI or not required for testing";
   composer remove almasaeed2010/adminlte --no-update
   composer remove yiisoft/yii2-debug phpunit/phpunit phpunit/dbunit --dev --no-update
-  echo "downloading dependencies...";
+  echo "\e[43mdownloading dependencies...";
   composer install --prefer-dist
   composer require codeclimate/php-test-reporter --prefer-dist
 fi

f768ee775cf8844e384854a7c699d6f54f788177
Yurii K. <yurii-github@users.noreply.github.com>
2015-06-11 12:52:59 +0300

    [skip ci]

--- .travis-install.sh
+++ .travis-install.sh
@@ -4,6 +4,8 @@ if [ -d vendor/bin ]
 then
   echo "using cache. nothing to do";
 else
+  echo "getting latest PHPUnit";
+  wget https://phar.phpunit.de/phpunit.phar
   echo "removing dev deps as we have ones in CI or not required for testing";
   composer remove almasaeed2010/adminlte --no-update
   composer remove yiisoft/yii2-debug phpunit/phpunit phpunit/dbunit --dev --no-update

272360cc4a2ea272b17873f2d629f8b10b56c6b7
Yurii K. <yurii-github@users.noreply.github.com>
2015-06-11 12:39:34 +0300

    Update .travis-install.sh

--- .travis-install.sh
+++ .travis-install.sh
@@ -4,8 +4,8 @@ if [ -d vendor/bin ]
 then
   echo "using cache. nothing to do";
 else
-  echo "removing phpunit as we have once in CI";
-  composer remove phpunit/phpunit phpunit/dbunit --no-update  
+  echo "removing dev deps as we have ones in CI or not required for testing";
+  composer remove yiisoft/yii2-debug phpunit/phpunit phpunit/dbunit --no-update --no-interaction --dev
   echo "downloading dependencies...";
   composer install --prefer-dist
   composer require codeclimate/php-test-reporter --prefer-dist

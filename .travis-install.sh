e0cda78a09f063184f0890fecedd9ef47c4516c9
Yurii K <y.korotia@hotmail.com>
2015-06-11 13:39:29 +0300

    travis

--- .travis-install.sh
+++ .travis-install.sh
@@ -4,9 +4,8 @@ if [ -d vendor/bin ]
 then
   echo "using cache. nothing to do";
 else
-  mkdir yurii-env;
   echo "getting latest PHPUnit";
-  wget https://phar.phpunit.de/phpunit.phar -O phpunit.phar -P yurii-env
+  wget https://phar.phpunit.de/phpunit.phar -O phpunit.phar -P yurii
   echo "removing dev deps as we have ones in CI or not required for testing";
   composer remove almasaeed2010/adminlte --no-update
   composer remove yiisoft/yii2-debug phpunit/phpunit phpunit/dbunit --dev --no-update

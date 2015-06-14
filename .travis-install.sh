ee3313aa912f7a3c13d62b64f8a6e5093cbd01a1
Yurii K. <yurii-github@users.noreply.github.com>
2015-06-14 21:15:20 +0300

    Update .travis-install.sh

--- .travis-install.sh
+++ .travis-install.sh
@@ -7,8 +7,8 @@ else
   echo -e "\e[0;34;40m getting latest PHPUnit";
   wget https://phar.phpunit.de/phpunit.phar -O vendor/phpunit.phar --no-check-certificate
   echo -e "\e[0;34;40m removing dev deps as we have ones in CI or not required for testing";
-  composer remove almasaeed2010/adminlte --no-update
-  composer remove yiisoft/yii2-debug --no-update
+  composer remove almasaeed2010/adminlte --dev --no-update
+  composer remove yiisoft/yii2-debug --dev --no-update
   composer remove  phpunit/phpunit phpunit/dbunit --dev --no-update
   echo -e "\e[0;34;40m downloading dependencies...";
   composer require codeclimate/php-test-reporter --no-update

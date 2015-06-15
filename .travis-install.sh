8e462fba14e5e4462afe11dcd91124a4a5193e6e
Yurii K <y.korotia@hotmail.com>
2015-06-15 12:40:44 +0300

    skip clover functionality

--- .travis-install.sh
+++ .travis-install.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
+color="\e[0;34;40m";
+
+echo -e "${color}checking for [skip clover]..";
+if git log -1 --oneline | grep -iq "[skip clover]" 
+then
+  export CLOVER=''
+else
+  export CLOVER="--coverage-clover $HOME/build/logs/clover.xml";
+fi
+
+
 if [ -d vendor/bin ]
 then
-  echo -e "\e[0;34;40m using cache. nothing to do";
+  echo -e "${color}using cache. nothing to do";
 else
-  echo -e "\e[0;34;40m login to github via oauth token..";
-  composer config -g github-oauth.github.com $GITHUB_TOKEN
-  echo -e "\e[0;34;40m getting latest PHPUnit";
+  echo -e "${color}getting latest PHPUnit";
   wget https://phar.phpunit.de/phpunit.phar -O vendor/phpunit.phar --no-check-certificate
-  echo -e "\e[0;34;40m removing dev deps as we have ones in CI or not required for testing";
+  
+  echo -e "${color}setting github oauth token..";
+  composer config -g github-oauth.github.com $GITHUB_TOKEN
+
+  echo -e "${color}removing dev deps as we have ones in CI or not required for testing";
   composer remove almasaeed2010/adminlte --dev --no-update
   composer remove yiisoft/yii2-debug --dev --no-update
   composer remove  phpunit/phpunit phpunit/dbunit --dev --no-update
-  echo -e "\e[0;34;40m downloading dependencies...";
+  echo -e "${color}downloading required dependencies...";
   composer require codeclimate/php-test-reporter --no-update
   composer install --prefer-dist --optimize-autoloader --no-progress
-  echo -e "\e[0;34;40m installed dependencies:";
+  echo -e "${color}show installed dependencies:";
   composer show --installed
 fi

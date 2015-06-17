7e8381c34554bdf206bd6f92c6b0903e98693e73
Yurii K <y.korotia@hotmail.com>
2015-06-18 00:03:12 +0300

    tests

--- app/tests/unit/components/log/FileTargetTest.php
+++ app/tests/unit/components/log/FileTargetTest.php
@@ -3,17 +3,22 @@ namespace tests\components\log;
 
 use app\components\ApcCache;
 use yii\log\Logger;
+use app\components\log\FileTarget;
 
 class FileTargetTest extends \tests\AppTestCase
 {
+	protected function setUp()
+	{
+		parent::setUp();
+		$this->initAppFileSystem();
+	}
+	
 	public function test_formatMessage()
 	{
 		//txt, lvl, category, timestamp
 		$message =	['some message', Logger::LEVEL_INFO, 'app', (new \DateTime('2001-02-03 04:05:06'))->getTimestamp()+0.12345];
-		
-		$this->initAppFileSystem();
 		$ft = new \yii\log\FileTarget(['logFile' => '@app/runtime/logs/log.txt']);
-		$ft_my = new \app\components\log\FileTarget(['logFile' => '@app/runtime/logs/log_my.txt']);
+		$ft_my = new FileTarget(['logFile' => '@app/runtime/logs/log_my.txt']);
 
 		$this->assertEquals('2001-02-03 04:05:06 [info][app] some message', $ft->formatMessage($message)); //yii2 default
 		$ft_my->with_microtime = false;
@@ -26,11 +31,10 @@ class FileTargetTest extends \tests\AppTestCase
 	public function test_formatMessage_varDump()
 	{
 		$message =	[ ['key' => 'value'], Logger::LEVEL_INFO, 'app', (new \DateTime('2001-02-03 04:05:06'))->getTimestamp()+0.12345];
+		$ft_my = new FileTarget(['logFile' => '@app/runtime/logs/log_my.txt']);
+		$ft_my->with_microtime = true;
 		
-		$this->initAppFileSystem();
-		$ft_my = new \app\components\log\FileTarget(['logFile' => '@app/runtime/logs/log_my.txt']);
-		
-		$this->assertEquals("2001-02-03 04:05:06 [info][app] [
+		$this->assertEquals("2001-02-03 04:05:06:123450 [info][app] [
     'key' => 'value',
 ]", $ft_my->formatMessage($message));
 		
@@ -38,12 +42,13 @@ class FileTargetTest extends \tests\AppTestCase
 	
 	public function test_formatMessage_Trace()
 	{
-		$message =	['some message', Logger::LEVEL_INFO, 'app', (new \DateTime('2001-02-03 04:05:06'))->getTimestamp()+0.12345, [['file' => '1', 'line' => '2']]];
-	
-		$this->initAppFileSystem();
-		$ft_my = new \app\components\log\FileTarget(['logFile' => '@app/runtime/logs/log_my.txt']);
-	
-		$this->assertEquals("2001-02-03 04:05:06 [info][app] some message
+		$message =	['some message', Logger::LEVEL_INFO, 'app', (new \DateTime('2001-02-03 04:05:06'))->getTimestamp()+0.12345, [
+			['file' => '1', 'line' => '2']
+		]];
+		$ft_my = new FileTarget(['logFile' => '@app/runtime/logs/log_my.txt']);
+		$ft_my->with_microtime = true;
+		
+		$this->assertEquals("2001-02-03 04:05:06:123450 [info][app] some message
     in 1:2", $ft_my->formatMessage($message));
 	
 	}

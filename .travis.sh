343242f1155a20d56f78dcacf5aa34eaa1fa233e
Yurii K. <yurii-github@users.noreply.github.com>
2015-09-17 10:32:52 +0300

    switch to 'phpunit-selenium' and tweak TravisCI setup to try selenium parallel testing

--- .travis.sh
+++ .travis.sh
@@ -3,7 +3,7 @@
 color="\e[0;34;40m";
 
 #
-# INSTALL 
+# INSTALL
 #
 if [ "$1" == "install" ]
 then
@@ -16,27 +16,35 @@ then
 		echo -e "${color}Loading cached apcu.so for PHP";
 		echo -e "extension = $(pwd)/vendor/apcu.so\napc.enabled=1\napc.enable_cli=1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 	else
+		echo -e "${color}getting latest Selenium Server Standalone";
+		wget http://goo.gl/PJUZfa -O vendor/selenium.jar
+		echo -e "${color}getting latest Chrome WebDriver for Selenium Server Standalone";
+		#http://chromedriver.storage.googleapis.com/2.20/chromedriver_linux64.zip
+		wget http://chromedriver.storage.googleapis.com/2.20/chromedriver_linux32.zip -O chrome32.zip
+		unzip -j chrome32.zip chromedriver
+		mv chromedriver vendor/chromedrv-32
+		echo -e "${color}getting latest PHPUnit";
 		# installing apcu. apcu 5+ not compatible with php 5.6. it will be loaded this time by pear
 		echo -e "${color}installing APCu 4.0.10 via PEAR/PECL...";
 		echo 'yes' | pecl install apcu-4.0.10
 		cp $(pear config-get ext_dir)/apcu.so $(pwd)/vendor/apcu.so
-		
+
 		echo -e "${color}getting latest PHPUnit...";
 		wget https://phar.phpunit.de/phpunit.phar -O vendor/phpunit.phar --no-check-certificate
-	  
+
 		echo -e "${color}setting github oauth token..";
 		composer config -g github-oauth.github.com $GITHUB_TOKEN
 
 		echo -e "${color}removing dev deps as we have ones in CI or not required for testing";
 		composer remove yiisoft/yii2-debug --dev --no-update
-		composer remove  phpunit/phpunit phpunit/dbunit --dev --no-update
+		composer remove phpunit/phpunit phpunit/dbunit phpunit/phpunit-selenium --dev --no-update
 		echo -e "${color}downloading required dependencies...";
 		composer require codeclimate/php-test-reporter --no-update
 		composer install --prefer-dist --optimize-autoloader --no-progress
 		echo -e "${color}show installed dependencies:";
 		composer show --installed
 	fi
-	
+
 	exit $?
 fi
 
@@ -54,7 +62,7 @@ then
 	php ../../vendor/phpunit.phar $CLOVER
 	export RES=$?
 	cd ../..
-	
+
 	exit $RES
 fi
 
@@ -71,7 +79,7 @@ then
 	else
 		echo -e "${color}skipping codeclimate reporter as clover was disabled by commit message or env SEND_CLOVER";
 	fi
-	
+
 	exit $?
 fi
 
--- app/tests/user/DummyTest.php
+++ app/tests/user/DummyTest.php
@@ -2,17 +2,17 @@
 
 class DummyTest extends PHPUnit_Extensions_Selenium2TestCase
 {
-	protected function setUp()
-	{
-		$this->setBrowser('chrome');
-		$this->setBrowserUrl('http://127.0.0.1:8888');
-	}
-	
-	public function testTitle()
-	{
-		$this->url('');
-		$this->assertEquals('MyLibrary ~ Books', $this->title());
-	}
-	
-	
+    protected function setUp()
+    {
+        $this->setBrowser('chrome');
+        $this->setBrowserUrl('http://127.0.0.1:8888');
+    }
+
+    public function testTitle()
+    {
+        $this->url('');
+        $this->assertEquals('MyLibrary ~ Books', $this->title());
+    }
+
+
 }

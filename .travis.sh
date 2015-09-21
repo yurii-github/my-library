4edbc058a6a5c93b3c85b9d495813fa0906f4334
Yurii K. <yurii-github@users.noreply.github.com>
2015-09-21 20:47:18 +0300

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -16,8 +16,8 @@ then
 		echo -e "${color}getting latest Selenium Server Standalone";
 		wget http://goo.gl/yLJLZg -O vendor/selenium-server-standalone.jar
 		echo -e "${color}getting latest Chrome WebDriver for Selenium Server Standalone";
-		wget http://chromedriver.storage.googleapis.com/2.9/chromedriver_linux32.zip -O vendor/chromedriver_linux32.zip
-		unzip vendor/chromedriver_linux32.zip -d vendor
+		wget http://chromedriver.storage.googleapis.com/2.9/chromedriver_linux32.zip -O chromedriver_linux32.zip
+		unzip chromedriver_linux32.zip -d vendor
 		
 		echo -e "${color}getting latest PHPUnit";
 		wget https://phar.phpunit.de/phpunit.phar -O vendor/phpunit.phar --no-check-certificate

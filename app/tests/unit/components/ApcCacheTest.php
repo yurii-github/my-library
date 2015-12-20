8e164bcc2cee95a6c4d1bb03e78dfef054798918
Yurii K. <yurii-github@users.noreply.github.com>
2015-12-20 15:17:17 +0200

    tests

--- app/tests/unit/components/ApcCacheTest.php
+++ app/tests/unit/components/ApcCacheTest.php
@@ -7,7 +7,7 @@ class ApcCacheTest extends \tests\AppTestCase
 {
 	public function test_buildKey()
 	{
-		if (!extension_loaded('apcu')) {
+		if (!extension_loaded('apc')) {
 			$this->markTestSkipped('APCu module is not loaded');
 		}
 		

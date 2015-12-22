86384d1bfeebe68fdb561239155a54ea4b8c7d54
Yurii K <yurii-github@users.noreply.github.com>
2015-12-22 14:44:43 +0200

    [ci skip]

--- .travis.sh
+++ .travis.sh
@@ -47,11 +47,14 @@ fi
 if [ "$1" == "script" ]
 then
 	cd app/tests
-	if [ "$SEND_CLOVER" != "1" ]
+	# if php5.6 use clover
+	if [ ${TRAVIS_PHP_VERSION:0:3} == "5.6" ] && [ ${DB_TYPE} = "sqlite" ]
 	then
-		export CLOVER=
+		php ../../vendor/phpunit.phar $CLOVER
+	else
+		php ../../vendor/phpunit.phar $CLOVER
 	fi
-	php ../../vendor/phpunit.phar $CLOVER
+
 	export RES=$?
 	cd ../..
 	
@@ -65,7 +68,7 @@ if [ "$1" == "after_success" ]
 then
 	# clover usage
 	#
-	if [ -n "$CLOVER" ] && [ "$SEND_CLOVER" = "1" ]
+	if [ ${TRAVIS_PHP_VERSION:0:3} == "5.6" ] && [ ${DB_TYPE} = "sqlite" ] && [ -n "$CLOVER" ]
 	then
 		vendor/bin/test-reporter
 	else

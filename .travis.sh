7bcce6a0948bbe2212567b9a65639e8bdbe1c15b
Yurii K. <yurii-github@users.noreply.github.com>
2015-12-23 20:12:26 +0200

    travis CI setup

--- .travis.sh
+++ .travis.sh
@@ -2,47 +2,106 @@
 
 color="\e[0;34;40m";
 
+function install()
+{
+	case $1 in
+
+		phpunit*)
+			echo -e "${color}getting latest PHPUnit..."
+			wget https://phar.phpunit.de/phpunit.phar -O vendor/phpunit.phar --no-check-certificate
+			;;
+
+		selenium*)
+			echo -e "${color}getting latest Selenium Server Standalone";
+			wget http://goo.gl/PJUZfa -O vendor/selenium.jar
+			;;
+
+		apcu*)
+			echo -e "${color}installing APCu 4.0.10 via PEAR/PECL..."
+			echo -e "${color}NOTE! APCu 5+ not compatible with php 5.6. SO we install 4.x"
+			echo 'yes' | pecl install apcu-4.0.10
+			cp $(pear config-get ext_dir)/apcu.so $(pwd)/vendor/apcu.so
+			;;
+
+		chromedriver*)
+			echo -e "${color}Installing Google Chrome...";
+			wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb --no-check-certificate
+			ar vx chrome.deb
+			mkdir yk_chrome
+			tar -xf data.tar.xz -C yk_chrome
+			mv yk_chrome vendor
+			#
+			echo -e "${color}Getting latest Chrome WebDriver for Selenium Server Standalone";
+			wget http://chromedriver.storage.googleapis.com/2.20/chromedriver_linux64.zip -O chrome.zip
+			unzip -j chrome.zip chromedriver
+			mv chromedriver vendor/chromedrv
+			chmod +x vendor/chromedrv
+			;;
+
+		deps*)
+			echo -e "${color}removing dev deps as we have ones in CI or not required for testing";
+			composer remove yiisoft/yii2-debug --dev --no-update
+			composer remove phpunit/phpunit phpunit/dbunit --dev --no-update
+			echo -e "${color}downloading required dependencies...";
+			composer require codeclimate/php-test-reporter --no-update
+			composer require codeclimate/php-test-reporter --no-update
+			composer require mikey179/vfsStream:1.5.0@stable --no-update
+			composer require facebook/webdriver:~1.0 --no-update
+			composer install --prefer-dist --optimize-autoloader --no-dev --no-progress
+			echo -e "${color}show installed dependencies:";
+			composer show --installed
+			;;
+
+		*)
+		echo 'Unknown parameter prived for instal()'
+		;;
+	esac
+}
+
+
 #
 # INSTALL
 #
 if [ "$1" == "install" ]
 then
+
+export PATH=$PATH:$(pwd)/vendor/yk_chrome/usr/bin
+vendor/yk_chrome/usr/bin/google-chrome-stable
+exit 300
+#echo "$(pwd)/vendor/yk_chrome/opt/google/chrome/google-chrome"  > /usr/bin/google-chrome-stable
+#chmod 777 /usr/bin/google-chrome-stable
+#
+#google-chrome-stable
+#vendor/yk_chrome/usr/bin/google-chrome-stable
+
+
 	# cache usage
 	#
 	if [ -d vendor/bin ]
 	then
-		echo -e "${color}Using cache. Nothing to do";
+		echo -e "${color}Using cache.";
 		#
 		echo -e "${color}Loading cached apcu.so for PHP";
 		echo -e "extension = $(pwd)/vendor/apcu.so\napc.enabled=1\napc.enable_cli=1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 	else
-		echo -e "${color}getting latest Selenium Server Standalone";
-		wget http://goo.gl/PJUZfa -O vendor/selenium.jar
-		echo -e "${color}getting latest Chrome WebDriver for Selenium Server Standalone";
-		#http://chromedriver.storage.googleapis.com/2.20/chromedriver_linux64.zip
-		wget http://chromedriver.storage.googleapis.com/2.20/chromedriver_linux32.zip -O chrome32.zip
-		unzip -j chrome32.zip chromedriver
-		mv chromedriver vendor/chromedrv-32
-		echo -e "${color}getting latest PHPUnit";
-		# installing apcu. apcu 5+ not compatible with php 5.6. it will be loaded this time by pear
-		echo -e "${color}installing APCu 4.0.10 via PEAR/PECL...";
-		echo 'yes' | pecl install apcu-4.0.10
-		cp $(pear config-get ext_dir)/apcu.so $(pwd)/vendor/apcu.so
+		if [ "${TRAVIS_PHP_VERSION:0:3}" != "5.6" ] || [ "${DB_TYPE}" != "sqlite" ]
+		then
+			echo -e "${color}Cache install is not allowed to not upload it in each parallel process. FIrst run is made in PHP-.6/sqlite . After its success please re-run tests"
+			exit 500
+		fi
 
-		echo -e "${color}getting latest PHPUnit...";
-		wget https://phar.phpunit.de/phpunit.phar -O vendor/phpunit.phar --no-check-certificate
-
-		echo -e "${color}setting github oauth token..";
+		echo -e "${color}Update Composer and set github oauth token..";
+		composer self-update
 		composer config -g github-oauth.github.com $GITHUB_TOKEN
 
-		echo -e "${color}removing dev deps as we have ones in CI or not required for testing";
-		composer remove yiisoft/yii2-debug --dev --no-update
-		composer remove phpunit/phpunit phpunit/dbunit --dev --no-update
-		echo -e "${color}downloading required dependencies...";
-		composer require codeclimate/php-test-reporter --no-update
-		composer install --prefer-dist --optimize-autoloader --no-progress
-		echo -e "${color}show installed dependencies:";
-		composer show --installed
+		install phpunit
+		install apcu
+		install selenium
+		install chromedriver
+		install deps
+
+		echo -e "${color}DEBUG: show vendor dir. IT will cached";
+		ls vendor -l
 	fi
 
 	exit $?
@@ -55,11 +114,14 @@ fi
 if [ "$1" == "script" ]
 then
 	cd app/tests
-	if [ "$SEND_CLOVER" != "1" ]
+	# if php5.6 use clover
+	if [ "${TRAVIS_PHP_VERSION:0:3}" == "5.6" ] && [ "${DB_TYPE}" == "sqlite" ]
 	then
-		export CLOVER=
+		php ../../vendor/phpunit.phar $CLOVER
+	else
+		php ../../vendor/phpunit.phar
 	fi
-	php ../../vendor/phpunit.phar $CLOVER
+
 	export RES=$?
 	cd ../..
 
@@ -71,13 +133,12 @@ fi
 #
 if [ "$1" == "after_success" ]
 then
-	# clover usage
-	#
-	if [ -n "$CLOVER" ] && [ "$SEND_CLOVER" = "1" ]
+	# if php5.6 use clover
+	if [ "${TRAVIS_PHP_VERSION:0:3}" == "5.6" ] && [ "${DB_TYPE}" == "sqlite" ] && [ -n "$CLOVER" ]
 	then
 		vendor/bin/test-reporter
 	else
-		echo -e "${color}skipping codeclimate reporter as clover was disabled by commit message or env SEND_CLOVER";
+		echo -e "${color}skipping codeclimate reporter";
 	fi
 
 	exit $?
--- app/tests/user/DummyTest.php
+++ app/tests/user/DummyTest.php
@@ -7,28 +7,33 @@ use Facebook\WebDriver\Remote\DesiredCapabilities;
 
 class DummyTest extends \PHPUnit_Framework_TestCase
 {
-    public $selenium2_hub = 'http://localhost:4444/wd/hub';
-    public $website_base;
+	public $selenium2_hub = 'http://127.0.0.1:4444/wd/hub';
+	public $website_base; 
+	
+	protected function setUp()
+	{
+		//$this->website_base = 'http://127.0.0.1:8888';
+		//return;
+		
+		if (!empty(getenv('TRAVIS'))) { // running on TRAVIS CI
+			$this->website_base = 'http://127.0.0.1:8888';
+		} else {
+			$this->website_base = 'http://localhost/mylibrary-yii2/app/public/';
+		}
+		
+		$this->website_base = 'http://127.0.0.1:80/mylibrary-yii2/app/public/';
+		$this->website_base = 'http://127.0.0.1:80';
+	}
+	
+	public function testTitle()
+	{
 
-    protected function setUp()
-    {
-        if (getenv('TRAVIS')) { // running on TRAVIS CI
-            $this->website_base = 'http://127.0.0.1:8888';
-        } else {
-            $this->website_base = 'http://localhost/mylibrary-yii2/app/public/';
-        }
-
-    }
-
-    public function testTitle()
-    {
-
-        $driver = RemoteWebDriver::create($this->selenium2_hub, DesiredCapabilities::chrome());
-
-        $driver->get($this->website_base);
-        $this->assertEquals('MyLibrary ~ Books', $driver->getTitle());
-        $driver->close();
-    }
-
-
+		$driver = RemoteWebDriver::create($this->selenium2_hub, DesiredCapabilities::chrome());
+		//$driver->w
+		$driver->get($this->website_base);
+		$this->assertEquals('MyLibrary ~ Books', $driver->getTitle());
+		$driver->close();
+	}
+	
+	
 }

31ab2264b9e24b87ea4642d39a165907c2c461fe
Yurii K <yurii-github@users.noreply.github.com>
2016-01-12 19:03:37 +0200

    cleaning

--- app/tests/user/DummyTest.php
+++ app/tests/user/DummyTest.php
@@ -7,10 +7,6 @@ class DummyTest extends \tests\AppUserTestCase
 	public function test_Title()
 	{
 		$this->driver->get($this->getSiteUrl());
-		
-		var_dump($this->getSiteUrl());
-		
-		echo $this->driver->getPageSource(); die;
 		$this->assertEquals('MyLibrary ~ Books', $this->driver->getTitle());
 	}
 	

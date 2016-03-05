bc5e6098b8165c9b323250cf1e3f55ea9e94e546
Yurii K. <yurii-github@users.noreply.github.com>
2016-03-05 21:47:26 +0200

    minor fixes

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,7 +1,7 @@
 <div style="font-size: 12px; margin: auto; width: 300px; text-align: left; font-size: 14px;">
 	<h1 style="text-align: center; font-size: 20px; margin-bottom: 20px">About</h1>
 	<p>
-		<b><a href="https://github.com/yurii-github/php-mylibrary">MyLibrary</a></b>
+		<b><a href="https://github.com/yurii-github/yii2-mylib">MyLibrary</a></b>
 		 is a software to manage your book library.
 	</p>
 	<h1 style="text-align: center; font-size: 17px; margin: 20px 0">Used Sources &amp; Projects</h1>

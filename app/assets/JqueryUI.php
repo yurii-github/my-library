52105456b3147ffaf66a25e91ebfee0736686276
Yurii K <yurii-github@users.noreply.github.com>
2017-02-26 19:00:23 +0200

    paying with versions

--- app/assets/JqueryUI.php
+++ app/assets/JqueryUI.php
@@ -9,8 +9,8 @@ class JqueryUI extends AssetBundle
 	public function init()
 	{
 		$theme = \Yii::$app->mycfg->system->theme;
-		$this->js = ["https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"];
-		$this->css = ["https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/$theme/jquery-ui.css"];
+		$this->js = ["https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"];
+		$this->css = ["https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/$theme/jquery-ui.css"];
 	
 		parent::init();
 	}

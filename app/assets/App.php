b6c2a62eb5d076b379225eead66a438c49bd9bb4
Yurii K <yurii-github@users.noreply.github.com>
2017-02-26 17:51:54 +0200

    better comment

--- app/assets/App.php
+++ app/assets/App.php
@@ -7,7 +7,7 @@ class App extends AssetBundle
 	public $css = ['app/css/yui-reset-3.5.0.css', 'app/css/style.css'];
 	
 	public $depends = [
-	  //GII/YII BUG: is not loaded in Gii, so we force it to load
+	  //GII/YII BUG: asset override is not loaded in Gii, so we force it to load
 	  \yii\web\JqueryAsset::class,
 	  \yii\bootstrap\BootstrapAsset::class,
 	  \yii\bootstrap\BootstrapPluginAsset::class,

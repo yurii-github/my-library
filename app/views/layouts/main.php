4b75063a6c6940999883ad5b4a97dab283a15296
Yurii K. <yurii-github@users.noreply.github.com>
2019-11-11 20:56:43 +0200

    refactor

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -15,25 +15,22 @@ $this->title = "MyLibrary ~ {$this->title}";
   <?php echo Html::csrfMetaTags(); ?>
   <?php $this->head(); ?>
 
-  <script src="<?= Yii::getAlias("@web/3rd/jquery-2.2.4/jquery.min.js");?>"></script>
-
+  <link type="text/css" href="<?= Yii::getAlias('@web/3rd/yui-reset-3.5.0.css');?>" rel="stylesheet" />
   <link type="text/css" href="<?= Yii::getAlias('@web/3rd/jquery-ui-themes-1.12.1/themes/'.\Yii::$app->mycfg->system->theme.'/jquery-ui.css');?>" rel="stylesheet">
   <link type="text/css" href="<?= Yii::getAlias('@web/3rd/jquery-ui-themes-1.12.1/themes/'.\Yii::$app->mycfg->system->theme.'/theme.css');?>" rel="stylesheet">
-  <script src="<?= Yii::getAlias("@web/3rd/jquery-ui-1.12.1/jquery-ui.min.js");?>"></script>
-
   <link type="text/css" href="<?= Yii::getAlias('@web/3rd/fancybox-2.1.5/source/jquery.fancybox.css');?>" rel="stylesheet">
-  <script src="<?= Yii::getAlias("@web/3rd/fancybox-2.1.5/source/jquery.fancybox.pack.js");?>"></script>
-
   <link type="text/css" href="<?= Yii::getAlias('@web/3rd/raty-2.8.0/lib/jquery.raty.css');?>" rel="stylesheet" />
-  <script src="<?= Yii::getAlias("@web/3rd/raty-2.8.0/lib/jquery.raty.js");?>"></script>
-
   <link type="text/css" href="<?= Yii::getAlias('@web/3rd/jqgrid-4.6.0/ui.jqgrid.css');?>" rel="stylesheet" />
+  <link type="text/css" href="<?= Yii::getAlias('@web/assets/app/css/style.css');?>" rel="stylesheet">
+
+  <script src="<?= Yii::getAlias("@web/3rd/jquery-2.2.4/jquery.min.js");?>"></script>
+  <script src="<?= Yii::getAlias("@web/3rd/jquery-ui-1.12.1/jquery-ui.min.js");?>"></script>
+  <script src="<?= Yii::getAlias("@web/3rd/fancybox-2.1.5/source/jquery.fancybox.pack.js");?>"></script>
+  <script src="<?= Yii::getAlias("@web/3rd/raty-2.8.0/lib/jquery.raty.js");?>"></script>
   <script src="<?= Yii::getAlias("@web/3rd/jqgrid-4.6.0/jquery.jqGrid.min.js");?>"></script>
   <script src="<?= Yii::getAlias("@web/3rd/jqgrid-4.6.0/grid.locale-".(['uk-UA' => 'ua'][\Yii::$app->language] ?? 'en').".js");?>"></script>
+  <script src="<?= Yii::getAlias('@web/3rd/js.cookie.js');?>"></script>
 
-  <script src="<?= Yii::getAlias('@web/assets/js.cookie.js');?>"></script>
-
-  <link type="text/css" href="<?= Yii::getAlias('@web/assets/app/css/style.css');?>" rel="stylesheet">
 </head>
 <body>
 <?php $this->beginBody(); ?>

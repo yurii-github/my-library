30f9802eab4b66f8cbb3c318126214a400c558ce
Yurii K <yurii-github@users.noreply.github.com>
2020-11-28 16:11:02 +0200

    add env

--- public/index.php
+++ public/index.php
@@ -1,16 +1,19 @@
 <?php
 
-
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Symfony\Component\Dotenv\Dotenv;
 
-require __DIR__ . '/../vendor/autoload.php';
+define('BASE_DIR', dirname(__DIR__));
+require BASE_DIR . '/vendor/autoload.php';
+
+(new Dotenv())->load(BASE_DIR.'/.env');
 
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+    $response->getBody()->write("Hello world! {$_ENV['APP_NAME']}");
     return $response;
 });
 

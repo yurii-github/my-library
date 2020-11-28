492589f1d17339e067517529b571ae80f9f46efd
Yurii K <yurii-github@users.noreply.github.com>
2020-11-28 16:34:52 +0200

    added translator

--- public/index.php
+++ public/index.php
@@ -4,11 +4,20 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use Symfony\Component\Dotenv\Dotenv;
+use \Symfony\Component\Translation\Translator;
+use \Symfony\Component\Translation\Loader\PhpFileLoader;
 
 define('BASE_DIR', dirname(__DIR__));
+define('SRC_DIR', dirname(__DIR__) .'/src');
+
 require BASE_DIR . '/vendor/autoload.php';
 
 (new Dotenv())->load(BASE_DIR.'/.env');
+
+$translator = new Translator('en_US');
+//$translator->setLocale('uk_UA');
+$translator->addLoader('php', new PhpFileLoader());
+$translator->addResource('php', SRC_DIR .'/i18n/uk_UA.php', 'uk_UA');
 
 $app = AppFactory::create();
 $app->addErrorMiddleware($_ENV['APP_DEBUG'], true, true);

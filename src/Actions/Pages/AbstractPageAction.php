ffdd80bab1fa4f0f11a385e40f81db16f3bf3bf2
Yurii K. <yurii-github@users.noreply.github.com>
2021-01-12 22:23:07 +0200

    refactor actions

--- src/Actions/Pages/AbstractPageAction.php
+++ src/Actions/Pages/AbstractPageAction.php
@@ -18,20 +18,20 @@
  * along with this program.  If not, see http://www.gnu.org/licenses
  */
 
-namespace App\Actions;
+namespace App\Actions\Pages;
 
 use App\Configuration\Configuration;
 use Psr\Container\ContainerInterface;
-use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Symfony\Component\Translation\Translator;
 use Twig\Environment;
-use \App\Models\Category;
 
-class GetIndexPageAction
+
+abstract class AbstractPageAction
 {
     /** @var Configuration */
     protected $config;
+    /** @var Environment */
     protected $twig;
     protected $translator;
 
@@ -43,26 +43,28 @@ class GetIndexPageAction
         $this->translator = $container->get(Translator::class);
     }
 
-
-    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    
+    protected function render(ServerRequestInterface $request, string $view, array $data)
     {
         $uri = $request->getUri();
         $gridLocale = [
             'en_US' => 'en',
             'uk_UA' => 'ua',
         ];
-        $categories = Category::all();
-        $response->getBody()->write($this->twig->render('index.html.twig', [
-            'VERSION' => 'v.'.$this->config->getVersion(),
+        $baseData = [
             't' => $this->translator,
-            'categories' => $categories,
             'path' => $uri->getPath(),
             'baseUrl' => $uri->getScheme() . '://' . $uri->getAuthority(),
             'appTheme' => $this->config->getSystem()->theme,
-            'LANGUAGE' => $this->config->getSystem()->language,
             'gridLocale' => $gridLocale[$this->translator->getLocale()],
-        ]));
-
-        return $response;
+            'config' => $this->config,
+            'currentUrl' => $uri->getScheme() . '://' . $uri->getAuthority() . $uri->getPath(),
+            'APP_VERSION' => 'v.'.$this->config->getVersion(),
+            'APP_LANGUAGE' => $this->config->getSystem()->language,
+        ];
+        $data = array_merge($baseData, $data);
+        
+        return $this->twig->render($view, $data);
     }
+
 }

16e296802c47bf046d0254cf7df47aaaa0929c27
Yurii K <yurii-github@users.noreply.github.com>
2021-01-17 18:15:25 +0200

    porting tests, add fixes along the way

--- tests/Functional/ManageBookActionTest.php
+++ tests/Functional/ManageBookActionTest.php
@@ -70,7 +70,7 @@ class ManageBookActionTest extends AbstractTestCase
         $this->assertDatabaseMissing('books', ['book_guid' => $book->book_guid]);
     }
 
-    
+
     public function testChangeFilenameIsSkipped()
     {
         $createdAt = Carbon::now();
@@ -123,7 +123,7 @@ class ManageBookActionTest extends AbstractTestCase
         ]);
     }
 
-    
+
     public function testCannotChangeFilenameFromTitleWithoutFileWithSync()
     {
         $this->setBookLibrarySync(true);
@@ -147,95 +147,42 @@ class ManageBookActionTest extends AbstractTestCase
             'filename' => $book->filename
         ]);
     }
-    
-    
-    /**
-     * @dataProvider provideSyncModes
-     *
-     * ACTION MUST:
-     *
-     * 1. not allow changes of book_guid, created and updated date, filename
-     * 2. generate filename based on title
-     * 3. generate updated_date
-     * 4. 
-     */
-    public function __test_action_Manage_Edit(bool $syncMode)
+
+
+    public function testCannotChangeFilenameFromTitleWithFileWithSync()
     {
-        $this->setBookLibrarySync($syncMode);
+        $this->setBookLibrarySync(false);
         $books = $this->populateBooks();
         $book = $books[0];
 
-        Carbon::setTestNow(Carbon::now());
+        $config = $this->app->getContainer()->get(Configuration::class);
+        assert($config instanceof Configuration);
+
+        file_put_contents($config->getFilepath($book->filename), 'sample-data');
+        $this->assertFileExists($config->getFilepath($book->filename));
+        $this->assertStringEqualsFile($config->getFilepath($book->filename), 'sample-data');
+
+        $this->setBookLibrarySync(true);
+
         $request = $this->createJsonRequest('POST', '/api/book/manage', [
-            'title' => 'title book #1',
-            'read' => 'no',
-            'favorite' => 0,
+            'id' => $book->book_guid,
+            'title' => 'new title X',
             'oper' => 'edit'
         ]);
+
         $response = $this->app->handle($request);
-
-
-        // CONFIGURE
-        $book = $this->books['inserted'][0];
-        $book_expected = $this->books['expected'][0];
-
-        $filename_expected = $filename_old = \Yii::$app->mycfg->library->directory . $book['filename'];
-        file_put_contents($filename_expected, 'sample-data');
-
-        $_SERVER['REQUEST_METHOD'] = 'POST';
-        $_POST['oper'] = 'edit';
-        $_POST['id'] = $book['book_guid'];
-        $_POST['created_date'] = '2000-01-01';
-        $_POST['updated_date'] = '2000-01-01';
-        $_POST['filename'] = '2000-01-01';
-
-        // - - - - - -
-
-
-        $this->controllerApiBook->runAction('manage');
-        $book_expected['filename'] = ", ''title book #1'',  [].";
-
-        // #1
-        // WORKAROUND FOR TRAVIS
-        $dt = new \DateTime();
-        $dt->setTimezone(new \DateTimeZone(\Yii::$app->getTimeZone()));
-        $book_expected['updated_date'] = $dt->format('Y-m-d H:i:s');
-
-        //CHECKING
-        /* @var $book_current \yii\db\BaseActiveRecord */
-        $book_current = Books::findOne(['book_guid' => $book['book_guid']]);
-
-        // #2
-        // WORKAROUND FOR TRAVIS: remove seconds, as it fails on slow machines, definely fails on Travis
-        $book_expected['updated_date'] = (new \DateTime($book_expected['updated_date']))->format('Y-m-d H:i');
-        $book_current['updated_date'] = (new \DateTime($book_current['updated_date']))->format('Y-m-d H:i');
-
-        // #3
-        $book_current_arr = $book_current->getAttributes();
-        $keys = array_keys($book_expected);
-        foreach ($keys as $k) {
-            if ($k == 'filename') { // skip filename checks here. checked at #4 below
-                continue;
-            }
-            $this->assertEquals($book_expected[$k], $book_current_arr[$k], "expected '$k' doesn't match");
-        }
-
-        // #4
-        if ($syncMode) { // file rename if sync ON
-            $filename_expected = \Yii::$app->mycfg->library->directory . $book_expected['filename']; // renamed new
-            $this->assertFileNotExists($filename_old); // old is not existed
-        }
-        $this->assertFileExists($filename_expected);
-        $this->assertEquals(file_get_contents($filename_expected), 'sample-data');
-    }
-
-
-    public function provideSyncModes()
-    {
-        return [
-            [true], // sync enabled
-            [false] // sync disabled
-        ];
+        $newFilename = ", ''new title X'',  [].";
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertSame('', (string)$response->getBody());
+        $this->assertDatabaseHas('books', [
+            'book_guid' => $book->book_guid,
+            'title' => 'new title X',
+            'filename' => $newFilename
+        ]);
+        // book file was moved
+        $this->assertFileNotExists($config->getFilepath($book->filename));
+        $this->assertFileExists($config->getFilepath($newFilename));
+        $this->assertStringEqualsFile($config->getFilepath($newFilename), 'sample-data');
     }
 
 

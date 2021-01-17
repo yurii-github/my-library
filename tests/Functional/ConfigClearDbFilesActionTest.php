600a5e331a266717da45188adc6fc3ede05b945d
Yurii K <yurii-github@users.noreply.github.com>
2021-01-18 01:24:03 +0200

    fix CI

--- tests/Functional/ConfigClearDbFilesActionTest.php
+++ tests/Functional/ConfigClearDbFilesActionTest.php
@@ -38,7 +38,10 @@ class ConfigClearDbFilesActionTest extends AbstractTestCase
         $response = $this->app->handle($request);
 
         $this->assertSame(200, $response->getStatusCode());
-        $this->assertJsonData([$books[1]->book_guid, $books[2]->book_guid], $response);
+        $this->assertEqualsCanonicalizing(
+            [$books[1]->book_guid, $books[2]->book_guid],
+            json_decode((string)$response->getBody(), true)
+        );
         $this->assertDatabaseCount('books', 1);
         $this->assertDatabaseHas('books', [
             'book_guid' => $book->book_guid,

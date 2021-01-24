4bf3a0112f68ae17f2b81042e597dd4b86d65193
Yurii K. <yurii-github@users.noreply.github.com>
2021-01-24 04:06:28 +0200

    more tests

---
+++ tests/Functional/ConfigGetBooksWithoutCoverActionTest.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Tests\Functional;
+
+use App\Models\Book;
+use Illuminate\Support\Collection;
+use Tests\PopulateBooksTrait;
+
+class ConfigGetBooksWithoutCoverActionTest extends AbstractTestCase
+{
+    use PopulateBooksTrait;
+
+    public function testWithoutPdfExtensionAndWithoutCover()
+    {
+        $this->populateBooks();
+
+        $request = $this->createRequest('GET', '/config/books-without-cover');
+        $response = $this->app->handle($request);
+
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertJsonData([], $response);
+    }
+
+    public function testWithoutPdfExtensionAndWithCover()
+    {
+        $books = $this->populateBooks();
+        foreach ($books as $book) {
+            $book->book_cover = 'some-data';
+            $book->save();
+        }
+
+        $request = $this->createRequest('GET', '/config/books-without-cover');
+        $response = $this->app->handle($request);
+        $content = (string)$response->getBody();
+
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertJsonData([], $response);
+    }
+
+    public function testWithPdfExtensionAndWithCover()
+    {
+        $books = $this->populateBooks();
+        foreach ($books as $book) {
+            $book->filename .= '.pdf';
+            $book->book_cover = 'some-data';
+            $book->save();
+        }
+
+        $request = $this->createRequest('GET', '/config/books-without-cover');
+        $response = $this->app->handle($request);
+        $content = (string)$response->getBody();
+
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertJsonData([], $response);
+    }
+
+    public function testWithPdfExtensionAndWithoutCover()
+    {
+        $books = $this->populateBooks();
+        foreach ($books as $book) {
+            $book->filename .= '.pdf';
+            $book->book_cover = null;
+            $book->save();
+        }
+        $arrayBooks = Collection::make($books)->map(function (Book $book) {
+            return ['filename' => $book->filename, 'book_guid' => $book->book_guid];
+        })->toArray();
+
+        $request = $this->createRequest('GET', '/config/books-without-cover');
+        $response = $this->app->handle($request);
+        $content = (string)$response->getBody();
+
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertEqualsCanonicalizing($arrayBooks, json_decode($content, true));
+    }
+
+    public function testWithPdfExtensionAndOneWithCover()
+    {
+        $books = $this->populateBooks();
+        foreach ($books as $book) {
+            $book->filename .= '.pdf';
+            $book->book_cover = null;
+            $book->save();
+        }
+        $bookWithCover = $books[1];
+        $bookWithCover->book_cover = 'some-data';
+        $bookWithCover->save();
+
+        $arrayBooks = Collection::make($books)->filter(function (Book $book) use ($bookWithCover) {
+            return $book->book_guid !== $bookWithCover->book_guid;
+        })->map(function (Book $book) {
+            return ['filename' => $book->filename, 'book_guid' => $book->book_guid];
+        })->toArray();
+
+        $request = $this->createRequest('GET', '/config/books-without-cover');
+        $response = $this->app->handle($request);
+        $content = (string)$response->getBody();
+
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertEqualsCanonicalizing($arrayBooks, json_decode($content, true));
+    }
+}

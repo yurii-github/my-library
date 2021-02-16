78d303685663abb5725efce21c92e7860622af14
Yurii K <yurii-github@users.noreply.github.com>
2021-02-16 12:10:51 +0200

    clean

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,18 +1,18 @@
 <?php
 
 /** @var \Composer\Autoload\ClassLoader $autoload */
-$autoload = require dirname(__DIR__).'/vendor/autoload.php';
+$autoload = require dirname(__DIR__) . '/vendor/autoload.php';
 
 // https://github.com/laravel/framework/tree/master/src/Illuminate/Testing/Constraints
 $autoload->addClassMap([
-    \Illuminate\Testing\Constraints\CountInDatabase::class => __DIR__ .'/Constraints/CountInDatabase.php',
-    \Illuminate\Testing\Constraints\HasInDatabase::class => __DIR__ .'/Constraints/HasInDatabase.php',
-    \Illuminate\Testing\InteractsWithDatabase::class => __DIR__ .'/Constraints/InteractsWithDatabase.php',
-    \Illuminate\Testing\Constraints\SoftDeletedInDatabase::class => __DIR__ .'/Constraints/SoftDeletedInDatabase.php',
+    \Illuminate\Testing\Constraints\CountInDatabase::class => __DIR__ . '/Constraints/CountInDatabase.php',
+    \Illuminate\Testing\Constraints\HasInDatabase::class => __DIR__ . '/Constraints/HasInDatabase.php',
+    \Illuminate\Testing\InteractsWithDatabase::class => __DIR__ . '/Constraints/InteractsWithDatabase.php',
+    \Illuminate\Testing\Constraints\SoftDeletedInDatabase::class => __DIR__ . '/Constraints/SoftDeletedInDatabase.php',
 ]);
 
-if (file_exists(__DIR__.'/local_test_env.php')) {
-    require __DIR__.'/test_env.php';
+if (file_exists(__DIR__ . '/local_test_env.php')) {
+    require __DIR__ . '/local_test_env.php';
 } else {
     if (!isset($_ENV['DB_TYPE'])) {
         putenv('DB_TYPE=sqlite_memory'); // sqlite_memory | sqlite | mysql

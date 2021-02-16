eba5b66929dc8968fec05a9e590d33abcca16ee8
Yurii K <yurii-github@users.noreply.github.com>
2021-02-17 00:42:53 +0200

    refactor

--- public/index.php
+++ public/index.php
@@ -5,7 +5,10 @@ use \App\PhpCliServer;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-Bootstrap::initEnvironment(dirname(__DIR__) . '/data');
+defined('DATA_DIR') || define('DATA_DIR', dirname(__DIR__) . '/data');
+defined('BASE_DIR') || define('BASE_DIR', dirname(__DIR__));
+defined('SRC_DIR') || define('SRC_DIR', BASE_DIR . '/src');
+defined('WEB_DIR') || define('WEB_DIR', BASE_DIR . '/public');
 
 if (PhpCliServer::handle(WEB_DIR)) {
     exit;

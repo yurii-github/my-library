db3c44de22863c00209e27403eb366db60ac10ac
Yurii K <yurii-github@users.noreply.github.com>
2021-02-18 14:55:06 +0200

    refactor import into new page

---
+++ public/assets/app/app.js
@@ -0,0 +1,27 @@
+
+// JQuery fix for empty body on JSON response
+$.ajaxSetup({
+  converters: {
+    'text json' : function(response) {
+      return (response === '') ? null : JSON.parse(response);
+    },
+  },
+});
+
+
+$.fn.extend({
+  setMsg: function (message, field, result = true) {
+    if (message === '') {
+      return
+    }
+
+    let state = (result === true ? 'highlight' : 'error')
+    let icon = (result === true ? 'info' : 'alert')
+
+    this.html(
+      '<div class="ui-state-' + state + ' ui-corner-all" style="padding: 10px; margin-top: 20px; margin-bottom: 20px;">' +
+      '<p><span class="ui-icon ui-icon-' + icon + '" style="float: left; margin-right: .3em;"></span> ' +
+      '<b>' + field + '</b>&nbsp;&nbsp; ' + message + '</p>' +
+      '</div>')
+  }
+})
